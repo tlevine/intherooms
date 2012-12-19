a7d7fcbab9a6c2dac8428a421e8a4cc26949fafa
Thomas Levine <occurrence@thomaslevine.com>
2012-12-19 09:59:18 -0500

    schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE meeting_search (
+  "Meeting Title" TEXT NOT NULL,
+  "Meeting Title Link" TEXT NOT NULL,
+  "Location" TEXT NOT NULL,
+  "Location Link" TEXT NOT NULL,
+  "Address" TEXT NOT NULL,
+  "Day" TEXT NOT NULL,
+  "Time" TEXT NOT NULL,
+  "Fellowship" TEXT NOT NULL,
+  UNIQUE("Meeting Title Link")
+);
+
+CREATE TABLE meeting_info (
+  "Url" TEXT NOT NULL,
+  "Meeting Description" TEXT NOT NULL,
+  FOREIGN KEY Url REFERENCES meeting_search("Meeting Title Link"),
+  UNIQUE("Meeting Title Link")
+);
+
+
+CREATE VIEW meeting AS
+SELECT *
+FROM meeting_search
+JOIN meeting_info ON "Url" = "Meeting Title Link";
