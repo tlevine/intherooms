59888136ca77390e7006bbcafd48ac8d7ea2d317
Thomas Levine <occurrence@thomaslevine.com>
2012-12-19 09:39:52 -0500

    parsing

--- parse_search.py
+++ parse_search.py
@@ -10,4 +10,18 @@ def has_correct_page_number(html, page_number):
     observed_page_numbers = set(map(int, html.xpath('//span[@class="current"]/text()')))
     return observed_page_numbers == {page_number}
 
-def has_c
+def table_data(html):
+    trs = html.cssselect('.all-meetings tr')
+    tr_head = trs[1]
+    trs_body = trs[1:]
+
+    keys = tr_head.xpath('th/text()')
+
+    data = []
+    for tr in trs_body:
+        values = [td.text_content for td in tr.cssselect('td')]
+        rowdata = OrderedDict(zip(keys, values))
+        rowdata['Meeting Title Link'] = tr.xpath('td[position()=1]/a/@href')
+        rowdata['Location Link'] = tr.xpath('td[position()=2]/a/@href')
+        data.append(rowdata)
+    return data
