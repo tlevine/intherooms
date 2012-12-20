747248cbba6947d61613a7089a6b63c428f5b86e
Thomas Levine <occurrence@thomaslevine.com>
2012-12-20 00:13:37 -0500

    fix meeting parser

--- parse_meeting.py
+++ parse_meeting.py
@@ -10,11 +10,18 @@ def meeting_info(html):
 
 def main():
     import sys
-    filename = os.path.join(os.environ['IN_THE_ROOMS_ROOT'], 'meetings', '%d.html' % _id(sys.argv[1]))
+    url = sys.argv[1]
+    filename = os.path.join(os.environ['IN_THE_ROOMS_ROOT'], 'meetings', '%d.html' % _id(url))
     dt = dumptruck.DumpTruck(
         dbname = os.path.join(os.environ['IN_THE_ROOMS_ROOT'], 'intherooms.db'),
         adapt_and_convert = True
     )
     html = lxml.html.parse(filename).getroot()
-    data = {'Meeting Description': meeting_info(html)}
+    data = {
+        'Url': url,
+        'Meeting Description': meeting_info(html),
+    }
     dt.insert(data, 'meeting_info')
+
+if __name__ == '__main__':
+    main()
