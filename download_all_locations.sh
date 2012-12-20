1509ad7c917c0a48d0b3495ddd2a5d2255d7361b
Thomas Levine <occurrence@thomaslevine.com>
2012-12-20 14:27:13 -0500

    typo

--- download_all_locations.sh
+++ download_all_locations.sh
@@ -4,7 +4,7 @@ db="$1"
 
 # Download locations based on those pages
 for location in $(sqlite3 "$db" 'SELECT "Location Link" FROM meeting_search'); do
-    if [ ! -e $IN_THE_ROOMS_ROOT/locations/$(echo "$location" | cut -d/ -f4).html ]; then
+    if [ ! -e $IN_THE_ROOMS_ROOT/locations/$(echo "$location" | cut -d/ -f5).html ]; then
         ./download_location.sh "$location"
         sleep 0s
     fi
