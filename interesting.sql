4b085f712c547cb1e66bf82f735f9cab74432406
Thomas Levine <occurrence@thomaslevine.com>
2012-12-20 14:32:50 -0500

    /oa//81760 does not show anything

--- interesting.sql
+++ interesting.sql
@@ -1,3 +1,4 @@
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/slaa/%';
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/alanon/%';
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/ga/Long%';
+SELECT * FROM meeting_search WHERE "Meeting Title Link" = '/oa//81760';
--- manual_fixes.sql
+++ manual_fixes.sql
@@ -1,7 +1,12 @@
+-- Newline in location link
 UPDATE meeting_search
 SET "Location Link" = '/locations/_/_/30016'
 WHERE "Location Link" LIKE '%30016';
 
+-- Newline in meeting link
 UPDATE meeting_search
 SET "Meeting Title Link" = '/ga/_/73215'
 WHERE "Meeting Title Link" LIKE '%73215';
+
+-- Page doesn't display meeting information
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/oa//81760', '');
