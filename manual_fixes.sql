9f701d96148d838fefdbea4759a012fd098783ca
Thomas Levine <occurrence@thomaslevine.com>
2012-12-20 14:35:45 -0500

    done with manual fixes

--- manual_fixes.sql
+++ manual_fixes.sql
@@ -10,3 +10,7 @@ WHERE "Meeting Title Link" LIKE '%73215';
 
 -- Page doesn't display meeting information
 INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/oa//81760', '');
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/aa//36357', '');
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/na//32908', '');
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/na//32973', '');
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('/na//33486', '');
