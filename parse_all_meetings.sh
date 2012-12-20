b9d20a28563c2bd272fc88eb410223352402022d
Thomas Levine <occurrence@thomaslevine.com>
2012-12-20 14:02:41 -0500

    details

--- parse_all_meetings.sh
+++ parse_all_meetings.sh
@@ -6,5 +6,8 @@ set -e
 # Parse the meetings.
 meetings_todo=$(sqlite3 "$1" 'SELECT "Meeting Title Link" FROM "meeting_search" WHERE "Meeting Title Link" NOT IN (SELECT "Url" FROM "meeting_info");')
 for url in $meetings_todo; do
-    ./parse_meeting.py "$url"
+    if ! ./parse_meeting.py "$url"; then
+        echo "Error for $url"
+        exit 1
+    fi
 done
