0028e861a2b6bd531c1aa613932d393ba4a266ce
Thomas Levine <occurrence@thomaslevine.com>
2012-12-23 16:28:47 -0500

    area code group

--- interesting.sql
+++ interesting.sql
@@ -2,4 +2,6 @@ SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE 
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/alanon/%';
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/ga/Long%';
 SELECT * FROM meeting_search WHERE "Meeting Title Link" = '/oa//81760';
-SELECT substr("Phone", 1, 3) AS 'Area Code' FROM meeting_info WHERE PHONE != '';
+
+SELECT substr("Phone", 1, 3) AS 'Area Code', count(*)
+FROM meeting_info WHERE PHONE != '' GROUP BY "Area Code";
