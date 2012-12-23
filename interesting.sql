3ce5a0c181982ba84f6f9c6f0560a098d8cfedb0
Thomas Levine <occurrence@thomaslevine.com>
2012-12-23 16:28:01 -0500

    area code diagnosis

--- interesting.sql
+++ interesting.sql
@@ -2,3 +2,4 @@ SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE 
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/alanon/%';
 SELECT "Meeting Title Link" FROM meeting_search WHERE "Meeting Title Link" LIKE '/ga/Long%';
 SELECT * FROM meeting_search WHERE "Meeting Title Link" = '/oa//81760';
+SELECT substr("Phone", 1, 3) AS 'Area Code' FROM meeting_info WHERE PHONE != '';
