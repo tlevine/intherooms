8dfb8e0790aa380ef66beb2fd93667579dd89221
Thomas Levine <occurrence@thomaslevine.com>
2013-01-18 18:09:17 -0500

    manual fixes

--- manual_fixes-sf.sql
+++ manual_fixes-sf.sql
@@ -6,3 +6,7 @@ WHERE "Meeting Title Link" LIKE '%72930';
 UPDATE meeting_search
 SET "Meeting Title Link" = '/ga/Eunice-GA-There-is-a-Better-Way/72599'
 WHERE "Meeting Title Link" LIKE '%72599';
+
+-- Page doesn't display meeting information
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('104917', '');
+INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('104918', '');
