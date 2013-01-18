b0b84382624d116c70df898b9cff5843d5351b03
Thomas Levine <occurrence@thomaslevine.com>
2013-01-18 16:32:28 -0500

    manual fix

--- manual_fixes-sf.sql
+++ manual_fixes-sf.sql
@@ -2,3 +2,7 @@
 UPDATE meeting_search
 SET "Meeting Title Link" = '/ga/Reno-GA/72930'
 WHERE "Meeting Title Link" LIKE '%72930';
+
+UPDATE meeting_search
+SET "Meeting Title Link" = '/ga/Eunice-GA-There-is-a-Better-Way/72599'
+WHERE "Meeting Title Link" LIKE '%72599';
