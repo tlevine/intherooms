ab749d8acf939d9be24142530d9a7f11e7841ab8
Thomas Levine <occurrence@thomaslevine.com>
2013-01-19 00:20:41 -0500

    manual_fixes

--- manual_fixes-sf.sql
+++ manual_fixes-sf.sql
@@ -1,4 +1,3 @@
--- Newline in meeting link
 UPDATE meeting_search
 SET "Meeting Title Link" = '/ga/Reno-GA/72930'
 WHERE "Meeting Title Link" LIKE '%72930';
@@ -6,6 +5,10 @@ WHERE "Meeting Title Link" LIKE '%72930';
 UPDATE meeting_search
 SET "Meeting Title Link" = '/ga/Eunice-GA-There-is-a-Better-Way/72599'
 WHERE "Meeting Title Link" LIKE '%72599';
+
+UPDATE meeting_search
+SET "Location Link" = '/locations/Oakland/Central-Office/45746'
+WHERE "Location Link" LIKE '%45746';
 
 -- Page doesn't display meeting information
 INSERT INTO meeting_info (Url, "Meeting Description") VALUES ('104917', '');
