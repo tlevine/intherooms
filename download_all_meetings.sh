4c5d2f106e071186c4ff113f66ef7bd4918dc695
Thomas Levine <occurrence@thomaslevine.com>
2013-02-26 20:22:14 -0500

    download meetings

--- download_all_meetings.sh
+++ download_all_meetings.sh
@@ -4,8 +4,11 @@ db="$1"
 
 # Download meetings based on those pages
 for meeting in $(sqlite3 "$db" 'SELECT "Meeting Title Link" FROM meeting_search'); do
-    if [ ! -e $IN_THE_ROOMS_ROOT/meetings/$(echo "$meeting" | cut -d/ -f4).html ]; then
-        ./download_meeting.sh "$meeting"
+    echo $meeting
+    meeting_number=$(echo "$meeting" | cut -d/ -f4)
+    if [ ! -e $IN_THE_ROOMS_ROOT/meetings/$meeting_number.html ]; then
+        ./download_meeting.sh "$meeting" ||
+        ./download_meeting.sh /_/"$meeting_number"
         sleep 0s
     fi
 done
