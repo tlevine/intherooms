90bd364586ad2041c31feb7996f172d5f74fd244
Thomas Levine <occurrence@thomaslevine.com>
2013-02-26 20:29:47 -0500

    aoeu

--- download_all_locations.sh
+++ download_all_locations.sh
@@ -4,8 +4,10 @@ db="$1"
 
 # Download locations based on those pages
 for location in $(sqlite3 "$db" 'SELECT "Location Link" FROM meeting_search'); do
-    if [ ! -e $IN_THE_ROOMS_ROOT/locations/$(echo "$location" | cut -d/ -f5).html ]; then
-        ./download_location.sh "$location"
+    location_number=$(echo "$location" | cut -d/ -f5)
+    if [ ! -e "$IN_THE_ROOMS_ROOT/locations/${location_number}.html" ]; then
+        ./download_location.sh "$location" ||
+        ./download_location.sh /locations/_/_/"$location_number"
         sleep 0s
     fi
 done
--- download_all_meetings.sh
+++ download_all_meetings.sh
@@ -4,11 +4,10 @@ db="$1"
 
 # Download meetings based on those pages
 for meeting in $(sqlite3 "$db" 'SELECT "Meeting Title Link" FROM meeting_search'); do
-    echo $meeting
     meeting_number=$(echo "$meeting" | cut -d/ -f4)
     if [ ! -e $IN_THE_ROOMS_ROOT/meetings/$meeting_number.html ]; then
         ./download_meeting.sh "$meeting" ||
-        ./download_meeting.sh /_/"$meeting_number"
+        ./download_meeting.sh /_/_/"$meeting_number"
         sleep 0s
     fi
 done
