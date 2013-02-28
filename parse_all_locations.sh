93eca6111a5809573ff6efb5b7290deddd6ba5d4
Thomas Levine <occurrence@thomaslevine.com>
2013-02-28 10:54:33 -0500

    avoid location link clash

--- parse_all_locations.sh
+++ parse_all_locations.sh
@@ -4,7 +4,7 @@ db="$1"
 set -e
 
 # Parse the locations
-locations_todo=$(sqlite3 "$1" 'SELECT "Location Link" FROM "meeting_search" WHERE "Location Link" NOT IN (SELECT "Url" FROM "location");')
+locations_todo=$(sqlite3 "$1" 'SELECT DISTINCT "Location Link" FROM "meeting_search" WHERE "Location Link" NOT IN (SELECT "Url" FROM "location");')
 for url in $locations_todo; do
     ./parse_location.py "$url"
 done
